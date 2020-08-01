fb5f352304335c5904f8014b269e64417d4edcbd
Spencer Felton <SpencerEFelton@gmail.com>
2020-08-01 22:32:30 +0100

    Update reverseString.py

--- Easy/Array/reverseString.py
+++ Easy/Array/reverseString.py
@@ -5,6 +5,6 @@ class Solution:
         """
         len_s = len(s)
         for x in range(0, len_s//2):
-                temp = s[x]
+                temp = s[x] # modifying in place so we need a temp value to not lose the stored values
                 s[x] = s[len_s-1-x]
-                s[len_s-1-x] = temp       
+                s[len_s-1-x] = temp
